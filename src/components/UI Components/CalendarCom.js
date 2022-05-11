575f76621c196bf50dc8ec04911d0fe50fcbc0c0
WiaanDuvenhage-200307 <200307@virtualwindow.co.za>
2022-05-11 17:51:41 +0200

    calendar days implemented

--- src/components/UI Components/CalendarCom.js
+++ src/components/UI Components/CalendarCom.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import {takeMonth} from '../calendar/CalendarMod';
+import { format } from 'date-fns';
+import {takeMonth} from '../module/CalendarMod';
+
+function WeekNames() {
+    return( 
+        <div className={"grid grid-cols-7"}>
+                    {
+                        ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map(dayName =>
+                        <div className={"bg-blue-200 h-16 w-16 flex items-center justify-center border border-blue-200"}>{dayName}</div>)
+                    }
+        </div>
+    )
+}
 
 const CalendarMod = () => {
    const data = takeMonth()();
     return (
-        <div className='calendar'>
-            {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map(dayName => <div className='columns'>{dayName}</div>)}
+       <div className={"bg-white box-border m-1 flex calendar"}>
+           <div className={"border round-md"}>
+                <WeekNames />
+                {
+                    data.map(week => <div className={"grid grid-cols-7"}>
+                        {
+                            week.map(day => <div className={"h-16 w-16 flex items-center justify-center border border-blue-200"}>{format(day, 'dd')}</div>)
+                        }
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
